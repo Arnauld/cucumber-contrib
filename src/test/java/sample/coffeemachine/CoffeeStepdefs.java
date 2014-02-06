1ae224ac0aaed482155d5788b5f78e12d4eee266
Arnauld Loyer <arnauld.loyer@gmail.com>
2014-02-06 11:28:55 +0100

    Cleanup and add sonatype requirement for maven central

--- src/test/java/sample/coffeemachine/CoffeeStepdefs.java
+++ src/test/java/sample/coffeemachine/CoffeeStepdefs.java
@@ -1,11 +1,11 @@
 package sample.coffeemachine;
 
-import static org.fest.assertions.Assertions.assertThat;
-
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import cucumber.runtime.PendingException;
+
+import static org.fest.assertions.Assertions.assertThat;
 
 /**
  * @author <a href="http://twitter.com/aloyer">@aloyer</a>
