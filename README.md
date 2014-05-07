510d6ae314cb9e2d6cfb89323dd4214c31b664a0
Arnauld <arnauld.loyer@gmail.com>
2014-05-07 23:38:26 +0200

    Preparation for v0.0.3

--- README.md
+++ README.md
@@ -2,29 +2,21 @@ cucumber-contrib
 ================
 
 
-Maven Local Repository
-----------------------
+Maven dependencies
+------------------
 
-    mvn install:install-file  -Dfile=tmp/ditaa-0.9.jar \
-                              -DgroupId=ditaa \
-                              -DartifactId=ditaa \
-                              -Dversion=0.9\
-                              -Dpackaging=jar \
-                              -DlocalRepositoryPath=repo/
+    <properties>
+        <asciitech.version>0.0.5</asciitech.version>
+    </properties>
 
-    mvn install:install-file  -Dfile=tmp/gral-core-0.10.jar \
-                              -DgroupId=de.erichseifert \
-                              -DartifactId=gral \
-                              -Dversion=0.10\
-                              -Dpackaging=jar \
-                              -DlocalRepositoryPath=repo/
 
-    mvn install:install-file  -Dfile=tmp/VectorGraphics2D-0.9.1.jar \
-                              -DgroupId=de.erichseifert \
-                              -DartifactId=VectorGraphics2D \
-                              -Dversion=0.9.1\
-                              -Dpackaging=jar \
-                              -DlocalRepositoryPath=repo/
+    <dependency>
+        <groupId>org.technbolts</groupId>
+        <artifactId>asciitech</artifactId>
+        <classifier>third-parties</classifier>
+        <version>${asciitech.version}</version>
+    </dependency>
+
 
 * [Ditaa](http://ditaa.sourceforge.net/#usage)
 * [LaTex short math Guide](ftp://ftp.ams.org/ams/doc/amsmath/short-math-guide.pdf)
--- src/test/java/sample/coffeemachine/RunCucumberTest.java
+++ src/test/java/sample/coffeemachine/RunCucumberTest.java
@@ -2,11 +2,11 @@ package sample.coffeemachine;
 
 import cucumber.api.junit.Cucumber;
 import cucumber.contrib.formatter.pdf.PdfFormatter;
-
 import cucumber.contrib.junit.CommentDocBlockFilter;
 import cucumber.contrib.junit.CucumberExt;
 import cucumber.contrib.junit.CucumberExtOptions;
 import org.junit.runner.RunWith;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 
@@ -21,17 +21,17 @@ public class RunCucumberTest {
     public static class ConfiguredFormatter extends PdfFormatter {
         public ConfiguredFormatter(File reportDir) throws FileNotFoundException {
             super(reportDir, defaultConfiguration()
-                    .withMetaInformationsResources(RunCucumberTest.class, "00-meta.properties")
-                    // override some of the previous meta infomations
-                    .withReportFilename("coffee-machine-specs.pdf")
-                    .withAuthor("Arnauld")
-                    .withTitle("Coffee Machine Specification")
-                    .withPreambuleResource(RunCucumberTest.class, "00-preambule.md")
-                    .withDocumentMargin(10, 40)
-                    .withDescriptionMargin(0, 0, 5, 5)
-                    .withScenarioMargin(0, 0, 2, 5)
-                    .displayUri(false)
-                    .displayTags(false)
+                            .withMetaInformationsResources(RunCucumberTest.class, "00-meta.properties")
+                                    // override some of the previous meta infomations
+                            .withReportFilename("coffee-machine-specs.pdf")
+                            .withAuthor("Arnauld")
+                            .withTitle("Coffee Machine Specification")
+                            .withPreambuleResource(RunCucumberTest.class, "00-preambule.md")
+                            .withDocumentMargin(10, 40)
+                            .withDescriptionMargin(0, 0, 5, 5)
+                            .withScenarioMargin(0, 0, 2, 5)
+                            .displayUri(false)
+                            .displayTags(false)
             );
         }
     }
